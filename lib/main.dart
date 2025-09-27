3157ecb0258e0653f64b7081718eecda677d67c2
Dheeraj Mishra <dheeraj2595@gmail.com>
2025-09-27 18:15:43 +0530

    Added functionality to drawer and rearranged code for a cleaner view.

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ class MainPageState extends State<MainPage> {
   final List<WidgetBuilder> pages = [
     (context) => MyHomePage(title: 'home'),
     (context) => CategoryPage(title: 'category'),
+    (context) => VaultPage(title: 'vault'),
     (context) => NewNote(title: 'New note'),
   ];
 
@@ -65,6 +66,79 @@ class MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          ' - भँवरा - ',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSecondary,
+            fontFamily: 'Poppins',
+            fontSize: MediaQuery.of(context).size.width * 0.08,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+          textAlign: TextAlign.left,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.onPrimary,
+                    Theme.of(context).colorScheme.primary,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Hive",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: MediaQuery.of(context).size.width * 0.08,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.sticky_note_2_outlined)),
+              title: Text("All notes"),
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.category_outlined)),
+              title: Text("Categories"),
+              onTap: () {
+                _onItemTapped(1);
+
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.lock_clock_outlined)),
+              title: Text("Vault"),
+              onTap: () {
+                _onItemTapped(3);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.bar_chart_outlined)),
+              title: Text("Memory statistics"),
+            ),
+          ],
+        ),
+      ),
       body: pages[_selectedIndex](context),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
@@ -77,10 +151,7 @@ class MainPageState extends State<MainPage> {
             icon: Icon(Icons.category_rounded),
             label: "category",
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category_rounded),
-            label: "New note",
-          ),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: "New note"),
         ],
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bhan_wa_ra/pages/categories.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -8,66 +9,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(
-          ' - भँवरा - ',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onSecondary,
-            fontFamily: 'Poppins',
-            fontSize: MediaQuery.of(context).size.width * 0.08,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
-          ),
-          textAlign: TextAlign.left,
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(context).colorScheme.onPrimary,
-                    Theme.of(context).colorScheme.primary,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Hive",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.width * 0.08,
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: CircleAvatar(child: Icon(Icons.sticky_note_2_outlined)),
-              title: Text("All notes"),
-            ),
-            ListTile(
-              leading: CircleAvatar(child: Icon(Icons.category_outlined)),
-              title: Text("Categories"),
-            ),
-            ListTile(
-              leading: CircleAvatar(child: Icon(Icons.lock_clock_outlined)),
-              title: Text("Vault"),
-            ),
-            ListTile(
-              leading: CircleAvatar(child: Icon(Icons.bar_chart_outlined)),
-              title: Text("Memory statistics"),
-            ),
-          ],
-        ),
-      ),
       body: Column(
         children: [
           Padding(
